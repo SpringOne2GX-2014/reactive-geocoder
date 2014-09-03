e070c7a14438fb728c08e0b8f60729a5cd944cdf
Luke Daley <ld@ldaley.com>
2014-09-03 20:17:13 +1000

    Add some diagnostics to see where the time is being spent,

--- src/main/java/demo/ProcessorApplication.java
+++ src/main/java/demo/ProcessorApplication.java
@@ -9,19 +9,28 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import ratpack.error.ServerErrorHandler;
+import ratpack.exec.ExecInterceptor;
+import ratpack.exec.Execution;
 import ratpack.func.Action;
 import ratpack.handling.Chain;
 import ratpack.handling.Context;
+import ratpack.handling.Handler;
+import ratpack.handling.RequestOutcome;
+import ratpack.http.Request;
 import ratpack.jackson.Jackson;
 import ratpack.render.Renderer;
 import ratpack.render.RendererSupport;
 import ratpack.spring.annotation.EnableRatpack;
+import ratpack.util.internal.NumberUtil;
 import reactor.core.Environment;
 import reactor.core.Reactor;
 import reactor.core.spec.Reactors;
 import reactor.rx.Stream;
 import reactor.rx.spec.Streams;
 import reactor.spring.context.config.EnableReactor;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Configuration
 @ComponentScan
@@ -54,12 +63,24 @@ public class ProcessorApplication {
 
 	@Bean
 	public Action<Chain> handlers(ProcessorRestApi restApi) {
+		final AtomicInteger counter = new AtomicInteger();
 
 		return (chain) -> {
 			chain.register(registrySpec -> registrySpec.add(ServerErrorHandler.class, (context, exception) -> {
 				exception.printStackTrace();
 				context.render("error");
 			}));
+
+			chain.handler(ctx -> {
+				long startAt = System.nanoTime();
+				ctx.addInterceptor(new ProcessingTimingInterceptor(ctx.getRequest()), execution -> {
+					ctx.onClose(requestOutcome -> {
+						Timer timer = ctx.getRequest().get(Timer.class);
+						System.out.println(counter.getAndIncrement() + " - compute: " + timer.getComputeTime() + ", blocking: " + timer.getBlockingTime() + ", total: " + (NumberUtil.toMillisDiffString(startAt, requestOutcome.getClosedAt())));
+					});
+					ctx.next();
+				});
+			});
 
 			chain.get(ctx -> ctx.render(ctx.file("public/index.html")));
 
@@ -79,6 +100,50 @@ public class ProcessorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProcessorApplication.class, args);
+	}
+
+	public static class Timer {
+
+		private final AtomicLong totalCompute = new AtomicLong();
+		private final AtomicLong totalBlocking = new AtomicLong();
+		private boolean blocking;
+		private final ThreadLocal<Long> startedAt = new ThreadLocal<Long>() {
+			protected Long initialValue() {
+				return 0l;
+			}
+		};
+		public void start(boolean blocking) {
+			this.blocking = blocking;
+			startedAt.set(System.currentTimeMillis());
+		}
+		public void stop() {
+			long startedAtTime = startedAt.get();
+			startedAt.remove();
+			AtomicLong counter = blocking ? totalBlocking : totalCompute;
+			counter.addAndGet(startedAtTime > 0 ? System.currentTimeMillis() - startedAtTime : 0);
+		}
+		public long getBlockingTime() {
+			return totalBlocking.get();
+		}
+		public long getComputeTime() {
+			return totalCompute.get();
+		}
+	}
+
+	public static class ProcessingTimingInterceptor implements ExecInterceptor {
+		private final Request request;
+
+		public ProcessingTimingInterceptor(Request request) {
+			this.request = request;
+			request.register(new Timer());
+		}
+
+		public void intercept(ExecInterceptor.ExecType type, Runnable continuation) {
+			Timer timer = request.get(Timer.class);
+			timer.start(type.equals(ExecInterceptor.ExecType.BLOCKING));
+			continuation.run();
+			timer.stop();
+		}
 	}
 
 }
